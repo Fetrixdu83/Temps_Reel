81c988249494db9718da02bb46afe92135986f9a
root <root@insa-21114.insa-toulouse.fr>
2025-04-11 08:45:44 +0000

    todo add PIP

--- wo_mutex.c
+++ wo_mutex.c
@@ -8,17 +8,21 @@
 #include <sched.h>
 #include <assert.h>
 
-#define NUM_TASKS 2
+#define NUM_TASKS 3
 #define HIGH_PRIO 99
 #define MID_PRIO 50
 #define LOW_PRIO 1
 
+pthread_mutex_t mp =  PTHREAD_MUTEX_INITIALIZER;
+int counter_mutex = 0;
+
 struct timespec period[NUM_TASKS] = {
     {1, 0},      // Task 0: 1s period
-    {2, 0}       // Task 1: 2s period
+    {2, 0},       // Task 1: 2s period
+    {3, 0}       // Task 2: 3s period
 };
 
-int load[NUM_TASKS] = {1000000000, 2000000000};
+int load[NUM_TASKS] = {1000000000, 1000000000, 1000000000};
 pthread_barrier_t barrier;
 
 void* periodic_task(void* arg) {
@@ -34,11 +38,23 @@ void* periodic_task(void* arg) {
         clock_gettime(CLOCK_MONOTONIC, &start);
         
         // Simulate workload
-        volatile int counter = 0;
-        for(int i = 0; i < load[task_id]; i++) {
-            counter++;
-            
-        };
+         
+        
+        if (task_id!=1){
+            pthread_mutex_lock(&mp);
+            counter_mutex = 0;
+            for(int i = 0; i < load[task_id]; i++) {
+                counter_mutex++; 
+            }
+
+            pthread_mutex_unlock(&mp);
+        }
+        else{
+            int counter = 0;  
+            for(int i = 0; i < load[task_id]; i++) {
+                counter++;
+            }
+        }
         
         clock_gettime(CLOCK_MONOTONIC, &end);
         response_time = (end.tv_sec - start.tv_sec) * 1e9 + 
@@ -63,42 +79,50 @@ void* periodic_task(void* arg) {
 int main() {
     pthread_t threads[NUM_TASKS];
     int task_ids[NUM_TASKS];
-    pthread_attr_t attr;
-    struct sched_param param;
+    pthread_attr_t attr[NUM_TASKS];
+    struct sched_param param[NUM_TASKS+1];
+    
+    int mutex = pthread_mutex_init(&mp, NULL);
+    
+    pthread_mutexattr_setprotocol(&attr, PTHREAD_PRIO_INHERIT);
 
+    // Initialize mutex
+    pthread_mutex_init(&mp, &attr[NUM_TASKS]);
+    
     // Initialize barrier
     pthread_barrier_init(&barrier, NULL, NUM_TASKS);
 
     // Set main thread to real-time priority
-    param.sched_priority = HIGH_PRIO;
-    if(pthread_setschedparam(pthread_self(), SCHED_FIFO, &param)) {
+    param[NUM_TASKS].sched_priority = HIGH_PRIO;
+    if(pthread_setschedparam(pthread_self(), SCHED_FIFO, &param[NUM_TASKS])) {
         perror("Failed to set main thread priority");
         exit(EXIT_FAILURE);
     }
 
     for(int i = 0; i < NUM_TASKS; i++) {
         task_ids[i] = i;
-        pthread_attr_init(&attr);
-        pthread_attr_setinheritsched(&attr, PTHREAD_EXPLICIT_SCHED);
+        pthread_attr_init(&attr[i]);
+        pthread_attr_setinheritsched(&attr[i], PTHREAD_EXPLICIT_SCHED);
         
         // Set CPU affinity to processor 0
         cpu_set_t cpuset;
         CPU_ZERO(&cpuset);
         CPU_SET(0, &cpuset);  // Set to CPU 0
-        pthread_attr_setaffinity_np(&attr, sizeof(cpu_set_t), &cpuset);
+        pthread_attr_setaffinity_np(&attr[i], sizeof(cpu_set_t), &cpuset);
         
         // Set priorities
         switch(i) {
-            case 1: param.sched_priority = HIGH_PRIO; break;
-            case 0: param.sched_priority = MID_PRIO; break;
+            case 2: param[i].sched_priority = HIGH_PRIO; break;
+            case 1: param[i].sched_priority = MID_PRIO; break;
+            case 0: param[i].sched_priority = LOW_PRIO; break;
         }
-        pthread_attr_setschedparam(&attr, &param);
-        pthread_attr_setschedpolicy(&attr, SCHED_FIFO);
-        if(pthread_create(&threads[i], &attr, periodic_task, &task_ids[i])) {
+        pthread_attr_setschedpolicy(&attr[i], SCHED_FIFO);
+        pthread_attr_setschedparam(&attr[i], &param[i]);
+        if(pthread_create(&threads[i], &attr[i], periodic_task, &task_ids[i])) {
             perror("Failed to create thread");
             exit(EXIT_FAILURE);
         }
-        pthread_attr_destroy(&attr);
+        pthread_attr_destroy(&attr[i]);
     }
 
     // Wait for threads
